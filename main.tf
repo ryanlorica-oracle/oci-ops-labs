40b806a73cdb9cb36325d40f7d10ae25a94d54ed
ryanlorica-oracle <70230621+ryanlorica-oracle@users.noreply.github.com>
2022-03-01 14:43:49 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,9 +12,27 @@ provider "oci" {
   config_file_profile = "learn-terraform"
 }
 
-resource "oci_core_vcn" "internal" {
-  dns_label      = "internal"
+resource "oci_core_vcn" "example_vcn" {
+  dns_label      = "examplevcn"
   cidr_block     = "172.16.0.0/20"
   compartment_id = var.compartment_id
-  display_name   = "Terraform VCN"
+  display_name   = "Example VCN"
+}
+
+resource "oci_core_subnet" "example_subnet_one" {
+  vcn_id                      = oci_core_vcn.example_vcn.id
+  cidr_block                  = "172.16.0.0/24"
+  compartment_id              = var.compartment_id
+  display_name                = "Example Subnet 1"
+  prohibit_public_ip_on_vnic  = false
+  dns_label                   = "exsubnetone"
+}
+
+resource "oci_core_subnet" "example_subnet_two" {
+  vcn_id                      = oci_core_vcn.example_vcn.id
+  cidr_block                  = "172.16.1.0/24"
+  compartment_id              = var.compartment_id
+  display_name                = "Example Subnet 2"
+  prohibit_public_ip_on_vnic  = false
+  dns_label                   = "exsubnettwo"
 }
